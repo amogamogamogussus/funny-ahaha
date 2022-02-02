cad2214ffeb1693814d6d598d1bb064da391edbe
zoomer <97625333+amogamogamogussus@users.noreply.github.com>
2022-02-01 21:27:24 -0800

    Update yest.cpp

--- yest/yest.cpp
+++ yest/yest.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
-using std::cout;
-
 int main()
 {
-    cout << "Hello World!";
+	std::string a1[4] = { "i said 1-3(restart program)", "you chose a!", "you chose b!", "you chose c!" };
+	int ans;
+	std::cout << "choose an number out of 1 to 3 \n";
+	std::cin >> ans;
+	if (ans > sizeof(a1)) {
+		std::cout << "you chose a number higher than 3! you chose " << ans << " this does not exist on the array!";
+	}
+	else {
+		std::cout << a1[ans] << "\n";
+	}
+	
 }
